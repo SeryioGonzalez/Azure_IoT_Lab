ec7d0602037ec5e0a720e3ab4261bccbe7292bdb
SeryioGonzalez <sergio.gonzalez.jimenez@gmail.com>
2019-11-07 00:25:27 +0100

    Update iot-hub-client.py

--- iot-client/iot-hub-client.py
+++ iot-client/iot-hub-client.py
@@ -1,3 +1,4 @@
+import json
 import random
 import re
 import sys
@@ -5,54 +6,77 @@ import time
 
 from azure.iot.device import IoTHubDeviceClient, Message
 
-CONNECTION_STRING = sys.argv[1]
+AUX_CONNECTION_STRING = sys.argv[1]
 
-# Define the JSON message to send to IoT Hub.
-BASE_BODY_TEMPERATURE = 37.0
-BASE_HEART_RATE = 65
-MAXIMUM_BODY_TEMPERATURE = 40.0
-MAXIMUM_HEART_RATE = 150
+AUX_BASE_HEART_RATE = 65
+AUX_BASE_BODY_TEMPERATURE = 37.0
+AUX_MAXIMUM_BODY_TEMPERATURE = 40.0
 
-MSG_TXT = '{{"temperature": {temperature_value},"heart_rate": {heart_rate_value}}}'
+#SENSOR DATA WILL HOST SENSOR METRICS
+sensor_data = {}
 
-def validate_connection_string():
-    if not CONNECTION_STRING.startswith( 'HostName=' ):
+#METHOD FOR ONE METRIC
+def get_sensor_temperature():
+	temperature = AUX_BASE_BODY_TEMPERATURE + (random.random() * random.random() * 5)
+	
+	return temperature
+
+#METHOD FOR ONE METRIC
+def get_sensor_heart_rate():
+	heart_rate = AUX_BASE_HEART_RATE + (random.random() * random.random() * 15)
+	
+	return heart_rate
+	
+def aux_validate_connection_string():
+    if not AUX_CONNECTION_STRING.startswith( 'HostName=' ):
         print "ERROR  - YOUR IoT HUB CONNECTION STRING IS NOT VALID"
         print "FORMAT - HostName=your_iot_hub_name.azure-devices.net;DeviceId=your_device_name;SharedAccessKey=your_shared_access_key"
         sys.exit()
 
-def iothub_client_init():
-    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+def aux_iothub_client_init():
+    client = IoTHubDeviceClient.create_from_connection_string(AUX_CONNECTION_STRING)
     return client
 
 def iothub_client_telemetry_sample_run():
     try:
-        validate_connection_string()
-        client = iothub_client_init()
+        aux_validate_connection_string()
+        client = aux_iothub_client_init()
 
         print ( "IoT Hub Simulated body sensor" )
         print ( "Press Ctrl-C to exit" )
 
+	#IT WILL RUN FOREVER UNLESS YOU STOP IT
         while True:
-        # Build the message with simulated telemetry values.
-            temperature_measure = BASE_BODY_TEMPERATURE + (random.random() * random.random() * 5)
-            heart_rate_measure  = BASE_HEART_RATE + (random.random() * random.random() * 15)
-            msg_txt_formatted = MSG_TXT.format(temperature_value=temperature_measure, heart_rate_value=heart_rate_measure)
-            message = Message(msg_txt_formatted)
+        #COLLECTING SENSOR VALUES
+		#NEW METRIC COLLECTION SHOULD ADD CODE HERE
+            temperature_measure = get_sensor_temperature()
+            heart_rate_measure  = get_sensor_heart_rate()
+		
+		#STORING SENSOR VALUES IN DATA STRUCTURE
+		#NEW METRIC COLLECTION SHOULD ADD CODE HERE
+            sensor_data['temperature'] = temperature_measure
+            sensor_data['heart_rate']  = heart_rate_measure
 
-        # Add a custom application property to the message.
-            if temperature_measure > MAXIMUM_BODY_TEMPERATURE:
-              message.custom_properties["temperature_alert"] = "true"
+		#TRANFORMING IT TO JSON			
+            json_sensor_data = json.dumps(sensor_data)
+		#CREATING AN AZURE IOT MESSAGE OBJECT		
+            azure_iot_message = Message(json_sensor_data)
+
+        #ADDING A CUSTOM PROPERTY OF OUR CHOICE TO THE MESSAGE CALLED temperature_alert
+            if temperature_measure > AUX_MAXIMUM_BODY_TEMPERATURE:
+              azure_iot_message.custom_properties["temperature_alert"] = "true"
             else:
-              message.custom_properties["temperature_alert"] = "false"
+              azure_iot_message.custom_properties["temperature_alert"] = "false"
 
-            message.content_encoding='utf-8'
-            message.content_type='application/json'
+        #SETTING PROPER MESSAGE ENCODING
+            azure_iot_message.content_encoding='utf-8'
+            azure_iot_message.content_type='application/json'
 
-            # Send the message.
-            print( "Sending message: {}".format(message) )
-            client.send_message(message)
+        #SENDING THE MESSAGE
+            print( "Sending azure_iot_message: {}".format(azure_iot_message) )
+            client.send_message(azure_iot_message)
             print ( "Message successfully sent" )
+        #SLEEPING FOR A SECOND BEFORE RESTARTING
             time.sleep(1)
 
     except KeyboardInterrupt:
