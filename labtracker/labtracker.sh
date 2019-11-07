1799fec9915ab12b0c9227e4461086f56b8e82d4
seryio <siyito.lpgc@gmail.com>
2019-11-07 01:20:09 +0100

    modified Readme

--- labtracker/labtracker.sh
+++ labtracker/labtracker.sh
@@ -1,16 +1,46 @@
 #!/bin/bash
 
-numGroups=16
-groupNamePrefix="icailabgroup"
+numGroups=2
+groupNamePrefix="icaiiotlabgroup"
 region="westeurope"
 resultFile="/home/sergio/index.html"
+
+cat <<EOF > $resultFile
+<!DOCTYPE html>
+<html>
+<head>
+<title>Lab validator</title>
+<meta charset="UTF-8">
+<style>
+table, th, td {
+  border: 1px solid black;
+}
+</style>
+<script>
+	var td_array = document.getElementsByTagName('TD');
+	alert(td_array.length);
+
+	for (var i = 0; i < td_array.length; i++){
+	  if (td_array[i].textContent == 'OK'){
+		td_array[i].style.color = 'red';
+	  };
+	};
+</script>
+</head>
+<body>
+<table><tr><th>GroupID</th><th>Azure IoT Hub</th><th>Device VM</th><th>Storage Account</th></tr>
+EOF
+
 
 function checkDomain {
 	domain=$1
 
 	response=$(nslookup $domain | egrep -o "canonical|NXDOMAIN")
 
-	if [ $response = "canonical" ]
+	if [ $response"a" = "canonicala" ]
+	then
+		echo "OK"
+	elif [ $response"a" = "a" ]
 	then
 		echo "OK"
 	else
@@ -45,7 +75,6 @@ function checkDeviceVM {
 	
 }
 
-echo "<table><tr><th>GroupID</th><th>Azure IoT Hub</th><th>Device VM</th><th>Storage Account</th></tr>" > $resultFile
 
 for groupId in $(seq 1 $numGroups)
 do
@@ -57,4 +86,4 @@ do
 
 done
 
-echo "</table>" >> $resultFile
+echo "</table></body></html>" >> $resultFile
